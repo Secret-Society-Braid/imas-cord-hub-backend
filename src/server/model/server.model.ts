36fbea905ba02dd2b32e5688e924ac1338a33db4
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-07-14 14:41:24 +0900

    add dynamic path

---
+++ src/server/model/server.model.ts
@@ -0,0 +1,14 @@
+import { serverType } from "../interface/server.interface";
+
+export const serverModel: Array<serverType> = [
+  {
+    id: 0,
+    name: 'testing',
+    ip: 'joint',
+    waifu: [
+      'testWaifu',
+    ],
+    description: 'testing server',
+    invite: 'https://example.com/invite/0',
+  }
+]
--- src/server/server.service.ts
+++ src/server/server.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { toStringify } from 'src/util/jsonUtil';
 import { serverType } from './interface/server.interface';
+import { serverModel } from './model/server.model';
 
 @Injectable()
 export class ServerService {
-  private readonly server: Array<serverType> = [];
+  private readonly server: Array<serverType> = [...serverModel];
 
   getAll(): string {
-    return toStringify({
-      message: "Welcome to the API of the imas-cord-hub-backend!",
-      location: "server"
-    });
+    return toStringify(this.server);
+  }
+
+  getById(id: string): string {
+    return toStringify(this.server.find(server => server.id === Number(id)));
   }
 }
