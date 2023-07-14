946017948be0c71c8048a577e318b88c529d088c
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-07-14 18:32:33 +0900

    update server definition

--- src/server/server.controller.ts
+++ src/server/server.controller.ts
@@ -1,21 +1,21 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ServerService } from './server.service';
 
-@Controller()
+@Controller('server')
 export class ServerController {
   constructor(private readonly serverService: ServerService) {}
 
-  @Get('server')
+  @Get()
   getAll(): string {
     return this.serverService.getAll();
   }
 
-  @Get('server/:id')
+  @Get(':id')
   getById(@Param('id') id: string): string {
     return this.serverService.getById(id);
   }
 
-  @Get('server/search/:term')
+  @Get('search/:term')
   searchByTerm(@Query('searchType') searchType: string, @Param('term') term: string): string {
     return this.serverService.searchByTerm(searchType, term);
   }
