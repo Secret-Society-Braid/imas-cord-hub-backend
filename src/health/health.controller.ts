85a9e33c2fd6df3dd4e8a26c15383b7de20cb985
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-07-17 20:56:43 +0900

    fix controllers to specify the content type

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { HealthService } from './health.service';
 
 @Controller('health')
@@ -6,6 +6,7 @@ export class HealthController {
   constructor (private readonly healthService: HealthService) {}
 
   @Get()
+  @Header('Content-Type', 'application/json')
   getHealth(): string {
     return this.healthService.getHealth();
   }
--- src/server/server.controller.ts
+++ src/server/server.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Header, Param, Query } from '@nestjs/common';
 import { ServerService } from './server.service';
 
 @Controller('server')
@@ -6,16 +6,19 @@ export class ServerController {
   constructor(private readonly serverService: ServerService) {}
 
   @Get()
+  @Header('Content-Type', 'application/json')
   getAll(): string {
     return this.serverService.getAll();
   }
 
   @Get(':id')
+  @Header('Content-Type', 'application/json')
   getById(@Param('id') id: string): string {
     return this.serverService.getById(id);
   }
 
   @Get('search/:term')
+  @Header('Content-Type', 'application/json')
   searchByTerm(@Query('searchType') searchType: string, @Param('term') term: string): string {
     return this.serverService.searchByTerm(searchType, term);
   }
