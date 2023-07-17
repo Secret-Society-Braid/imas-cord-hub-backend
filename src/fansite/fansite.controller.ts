57d3c258ee8cb2460e21280df13f9d210d0438b8
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-07-17 20:59:30 +0900

    fix controllers to specify the content type (#12)

--- src/fansite/fansite.controller.ts
+++ src/fansite/fansite.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Header, Param, Query } from '@nestjs/common';
 import { FansiteService } from './fansite.service';
 
 @Controller('fansite')
@@ -6,16 +6,19 @@ export class FansiteController {
   constructor(private readonly fansiteService: FansiteService) {}
 
   @Get()
+  @Header('Content-Type', 'application/json')
   getAll(): string {
     return this.fansiteService.getAll();
   }
 
   @Get(':id')
+  @Header('Content-Type', 'application/json')
   getById(@Param('id') id: string): string {
     return this.fansiteService.getById(id);
   }
 
   @Get('search/:term')
+  @Header('Content-Type', 'application/json')
   searchByTerm(@Query('searchType') searchType: string, @Param('id') id: string): string {
     return this.fansiteService.searchByTerm(searchType, id);
   }
