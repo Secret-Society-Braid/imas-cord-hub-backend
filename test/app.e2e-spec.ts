857638e4371a90acce88589237fcc58d0d9cf414
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-07-28 22:11:50 +0900

    add e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { json } from 'stream/consumers';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -19,6 +20,12 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect(JSON.stringify({ message: 'Welcome to the API of the imas-cord-hub-backend!'}, null, 2));
   });
+
+  it('/health (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/health')
+      .expect(200);
+  })
 });
