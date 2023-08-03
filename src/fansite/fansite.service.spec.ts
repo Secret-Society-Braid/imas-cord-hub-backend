0db775bf6835b8466366c1ccfba85522a59df25f
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-08-03 22:50:14 +0900

    add type information

--- src/fansite/fansite.service.spec.ts
+++ src/fansite/fansite.service.spec.ts
@@ -14,13 +14,13 @@ describe('FansiteService', () => {
   });
 
   it('getAll() should be return an Array of fansiteType', () => {
-    const res = service.getAll();
+    const res: Array<fansiteType> = service.getAll();
     expect(res).toBeInstanceOf(Array);
     expect(res[0]).toBeInstanceOf(Object);
   });
 
   it('getById() should be return a fansiteType', () => {
-    const res = service.getById('10422c16-d21c-444f-9897-11e2f2546357');
+    const res: fansiteType = service.getById('10422c16-d21c-444f-9897-11e2f2546357');
     expect(res).toBeInstanceOf(Object);
     expect(res.id).toBe('10422c16-d21c-444f-9897-11e2f2546357');
   });
@@ -30,7 +30,7 @@ describe('FansiteService', () => {
   });
 
   it('searchByTerm() should be return an Array of fansiteType', () => {
-    const res = service.searchByTerm(undefined, 'a');
+    const res: Array<fansiteType> = service.searchByTerm(undefined, 'a');
     expect(res).toBeInstanceOf(Array);
     expect(res[0]).toBeInstanceOf(Object);
   });
@@ -40,14 +40,14 @@ describe('FansiteService', () => {
   });
 
   it('getLatest() should be return a fansiteType', () => {
-    const res = service.getLatest();
+    const res: fansiteType = service.getLatest();
     expect(res.id).toBe('10422c16-d21c-444f-9897-11e2f2546357');
   });
 
   it('getRandom() should be return an Array of fansiteType', () => {
     for(let i = 0; i < 10; i++) {
       const randomLength = Math.floor(Math.random() * 10);
-      const res = service.getRandom(randomLength);
+      const res: Array<fansiteType> = service.getRandom(randomLength);
       expect(res.length).toBe(randomLength);
     }
   });
