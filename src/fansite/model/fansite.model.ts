f3a9ae7b28a27cc13f30ec265bd91bd47eabd3ea
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-08-03 18:44:41 +0900

    execute linting

--- src/fansite/model/fansite.model.ts
+++ src/fansite/model/fansite.model.ts
@@ -5,7 +5,8 @@ export const fansiteModel: Array<fansiteType> = [
     id: '10422c16-d21c-444f-9897-11e2f2546357',
     name: 'ImasCordHub',
     waifu: '',
-    description: 'あのDiscordサーバーも、そのファンサイトも、これひとつ。アイマス関連のDiscordサーバーとファンサイトをまとめたサイトです。',
-    link: 'https://imas-cord-hub.vercel.app/'
-  }
+    description:
+      'あのDiscordサーバーも、そのファンサイトも、これひとつ。アイマス関連のDiscordサーバーとファンサイトをまとめたサイトです。',
+    link: 'https://imas-cord-hub.vercel.app/',
+  },
 ];
