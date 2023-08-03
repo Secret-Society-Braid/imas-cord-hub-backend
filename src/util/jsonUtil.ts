f1d5bc78a78f75fa24156f030e95ebebec210171
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-08-03 18:57:00 +0900

    delete unnecessary util

--- src/util/jsonUtil.ts
+++
@@ -1,7 +0,0 @@
-export const toStringify = (obj: any): string => {
-  return JSON.stringify(obj, null, 2);
-};
-
-export const toStringifyMin = (obj: any): string => {
-  return JSON.stringify(obj, null, 0);
-};
