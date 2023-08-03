37ce14850c78238e1b9787250b31dbaa05d3f962
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-08-03 18:53:59 +0900

    delete outdated test

--- src/server/server.service.spec.ts
+++ src/server/server.service.spec.ts
@@ -13,37 +13,11 @@ describe('ServerService', () => {
     service = module.get<ServerService>(ServerService);
   });
 
-  it('server root array length has', () => {
-    const parsed: Array<serverType> = JSON.parse(
-      service.getAll(),
-    ) as Array<serverType>;
-    expect(parsed.length).toBe(1);
-  });
+  // Re-write tests:
+  // TODO: getAll()
+  // TODO: getById()
+  // TODO: searchByTerm()
+  // TODO: getLatest()
+  // TODO: getRandom()
 
-  it('server search path has to be array and should throw exception when invalid search type', () => {
-    const allData: Array<serverType> = JSON.parse(
-      service.getAll(),
-    ) as Array<serverType>;
-    const randomId: number = Math.floor(Math.random() * allData.length);
-    const parsed: Array<serverType> = JSON.parse(
-      service.searchByTerm('itself', allData[randomId].name),
-    ) as Array<serverType>;
-    expect(parsed.length).toBeGreaterThanOrEqual(1);
-    expect(() =>
-      service.searchByTerm('invalid', allData[randomId].name),
-    ).toThrow();
-  });
-
-  it('server latest path has to be object', () => {
-    const parsed: serverType = JSON.parse(service.getLatest()) as serverType;
-    expect(parsed.id).toBe('9bffce54-d95e-4f51-b8d1-8b68aaea605a');
-  });
-
-  it('server random path has to be array', () => {
-    const randomLength: number = Math.floor(Math.random() * 10);
-    const parsed: Array<serverType> = JSON.parse(
-      service.getRandom(randomLength),
-    ) as Array<serverType>;
-    expect(parsed.length).toBe(randomLength);
-  });
 });
