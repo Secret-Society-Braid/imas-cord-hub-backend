b885139fabbaed8b5746496bc287a16ee9319966
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-08-04 10:55:57 +0900

    fix tests

--- src/version/version.controller.spec.ts
+++ src/version/version.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VersionController } from './version.controller';
+import { VersionService } from './version.service';
 
 describe('VersionController', () => {
   let controller: VersionController;
@@ -7,6 +8,7 @@ describe('VersionController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [VersionController],
+      providers: [VersionService],
     }).compile();
 
     controller = module.get<VersionController>(VersionController);
